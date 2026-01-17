2d731d6e45bcfd791b26e1a77ea731c87cb41aad
maheshsudar <51480820+maheshsudar@users.noreply.github.com>
2026-01-17 14:10:55 +0100

    Delete app directory

--- app/mdm-approval/webapp/ext/controller/ListReportExt.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([
-    "sap/ui/core/mvc/ControllerExtension"
-], function (ControllerExtension) {
-    "use strict";
-
-    return ControllerExtension.extend("com.company.mdmapproval.ext.controller.ListReportExt", {
-        // Empty extension - required by manifest.json
-    });
-});
--- app/services.cds
+++
@@ -1,7 +0,0 @@
-
-using from './salesforce-requests/annotations';
-using from './coupa-requests/annotations';
-using from './pi-requests/annotations';
-using from './mdm-approval/annotations';
-using from './satellite-ack/annotations';
-using from './admin-config/annotations';
